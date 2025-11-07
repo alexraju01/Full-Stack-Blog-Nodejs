8c69fff042cb06f36a6effd5a731ac8d9c032a77
CodeExcalibar <Alex.raju1010@gmail.com>
2025-11-07 16:53:47 +0000

    Created basic server and listening on port 8000

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+// Development Logging
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+// app.use("/api/v1/blog", blogRouter);
+
+// Catches all undefined routes
+app.get("/*splat", async (req, res, next) => {
+	next(new AppError(`can't find the ${req.originalUrl} on the this server`));
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+	console.log(` Server is running at http://localhost:${PORT}`);
+});
+
+// module.exports = app;
