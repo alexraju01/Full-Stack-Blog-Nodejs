188721792b86da46419a61057c60f022fb69010c
CodeExcalibar <Alex.raju1010@gmail.com>
2025-11-09 13:23:35 +0000

    Improved update query

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -30,9 +30,13 @@ exports.GetOneBlog = async (req, res, next) => {
 
 exports.deleteBlog = async (req, res, next) => {
 	const { id } = req.params;
-	const blog = await Blog.destroy(id);
-
-	if (!blog) return next(new AppError("No blog found with that id", 404));
+	const deleteBlog = await Blog.destroy({
+		where: {
+			id,
+		},
+	});
+	console.log("deleted:", deleteBlog);
+	if (deleteBlog === 0) return next(new AppError("No blog found with that id", 404));
 
 	res.status(204).json({
 		status: "success",
@@ -40,15 +44,28 @@ exports.deleteBlog = async (req, res, next) => {
 	});
 };
 
-exports.updateBlog = async (req, res) => {
+exports.updateBlog = async (req, res, next) => {
 	const { id } = req.params;
-	const blog = await Blog.findByPk(id);
+	const updateData = req.body;
 
-	if (!blog) return next(new AppError("No blog found with that id", 404));
-	const updatedBlog = await blog.update(req.body);
+	const [affectedRows] = await Blog.update(updateData, {
+		where: {
+			id: id,
+		},
+	});
+	console.log("data:", affectedRows);
+
+	if (affectedRows === 0) {
+		return next(new AppError("No blog found with that id", 404));
+	}
+
+	const updatedBlog = await Blog.findByPk(id);
+	if (!updatedBlog) {
+		return next(new AppError("Blog was updated but could not be retrieved", 500));
+	}
 
 	res.status(200).json({
-		message: "Note updated successfully",
+		message: "success",
 		blog: updatedBlog,
 	});
 };
